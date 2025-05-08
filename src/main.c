8aea420384e6e6240df8fc891598be40089757ed
michalwa <michalwa2003@gmail.com>
2025-05-08 03:27:41 +0200

    fix: parse all node args correctly

--- src/main.c
+++ src/main.c
@@ -10,13 +10,13 @@
 #define ARGC_NTH_(_1, _2, _3, _4, _5, _6, _7, _8, N, ...) N
 #define ARGC_SEQ_                                         8, 7, 6, 5, 4, 3, 2, 1, 0
 
-void node_begin(int argc, node_type type, ...) {
+void node_begin(int indent, int argc, node_type type, ...) {
     va_list args;
     va_start(args, type);
 
     node node;
-    node.type = type;
 
+    if (argc-- > 0) node.type = type;
     if (argc-- > 0) node.token.type = va_arg(args, token_type);
     if (argc-- > 0) node.token.str = va_arg(args, char *);
     if (argc-- > 0) node.flags = va_arg(args, node_flags);
@@ -25,16 +25,18 @@ void node_begin(int argc, node_type type, ...) {
         node.str_parsed = (string){ str_parsed, strlen(str_parsed) };
     }
 
-    printf(F_NODE "\n", FA_NODE(node));
+    printf("%*s" F_NODE "\n", indent, "", FA_NODE(node));
 
     va_end(args);
 }
 
-void node_end(void) {}
-
 int main(void) {
-#define _BEGIN(...) node_begin(ARGC(__VA_ARGS__), __VA_ARGS__);
-#define _END        node_end();
+    int indent = 0;
+
+#define _BEGIN(...)                                     \
+    node_begin(indent, ARGC(__VA_ARGS__), __VA_ARGS__); \
+    indent += 2;
+#define _END indent -= 2;
     EXAMPLE_TREE
 #undef _BEGIN
 #undef _END
