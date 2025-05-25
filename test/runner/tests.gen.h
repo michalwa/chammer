4bbcf0e5dec4bed969fad98a4d9f47ad5eef3c31
michalwa <michalwa2003@gmail.com>
2025-05-25 20:58:53 +0200

    style: fix formatting

--- test/runner/tests.gen.h
+++ test/runner/tests.gen.h
@@ -1,7 +1,7 @@
-#define TESTS \
-    _(buffer_printf) \
-    _(buffer_putc) \
-    _(buffer_alloc) \
-    _(lexer_example) \
+#define TESTS                \
+    _(buffer_printf)         \
+    _(buffer_putc)           \
+    _(buffer_alloc)          \
+    _(lexer_example)         \
     _(parse_tuple_or_parens) \
     _(stack)
