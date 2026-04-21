69b57e58d5f815ddd20d1553eb60ec3708e04783
michalwa <michalwa2003@gmail.com>
2026-04-21 13:38:51 +0200

    fix tests

--- test/test_compiler.c
+++ test/test_compiler.c
@@ -5,29 +5,19 @@ TEST(compile_string) {
     Buffer out;
     buffer_init(&out);
 
-    size_t offset, len;
-
-    compile_string(STRING("\"\""), &out, &offset, &len);
+    compile_string(STRING("\"\""), &out);
     ASSERT_STR_EQ(out.data, "");
-    ASSERT_INT_EQ(offset, 0);
-    ASSERT_INT_EQ(len, 0);
     buffer_clear(&out);
 
-    compile_string(STRING("\"foo\""), &out, &offset, &len);
+    compile_string(STRING("\"foo\""), &out);
     ASSERT_STR_EQ(out.data, "foo");
-    ASSERT_INT_EQ(offset, 0);
-    ASSERT_INT_EQ(len, 3);
 
-    compile_string(STRING("\" bar\""), &out, &offset, &len);
+    compile_string(STRING("\" bar\""), &out);
     ASSERT_STR_EQ(out.data, "foo bar");
-    ASSERT_INT_EQ(offset, 3);
-    ASSERT_INT_EQ(len, 4);
     buffer_clear(&out);
 
-    compile_string(STRING("\"\\\"\\n\""), &out, &offset, &len);
+    compile_string(STRING("\"\\\"\\n\""), &out);
     ASSERT_STR_EQ(out.data, "\"\n");
-    ASSERT_INT_EQ(offset, 0);
-    ASSERT_INT_EQ(len, 2);
     buffer_clear(&out);
 
     buffer_free(&out);
