f7daa78932dd325e60cf8ca052090cf1bbbe1439
michalwa <michalwa2003@gmail.com>
2026-04-27 12:18:42 +0200

    format

--- lib/bytecode.h
+++ lib/bytecode.h
@@ -7,37 +7,35 @@
 
 #include "buffer.h"
 
-/*
- * Some opcodes expect extra bytes with arguments. Consult the `bytecode_put_*`
- * functions for specification.
- */
-#define EACH_OPCODE(_)                                                                             \
-    _(OP_JUMP, 0x01)      /* jump to instruction unconditionally */                                \
-    _(OP_JUMPIF, 0x02)    /* pop a value and jump to instruction if it's `true` */                 \
-    _(OP_JUMPIFN, 0x03)   /* pop a value and jump to instruction if it's `false` */                \
-    _(OP_CALL, 0x04)      /* push a function frame with N locals and jump to instruction */        \
-    _(OP_RETURN, 0x05)    /* return from OP_CALL */                                                \
-    _(OP_LOAD, 0x10)      /* load local */                                                         \
-    _(OP_STORE, 0x11)     /* store local */                                                        \
-    _(OP_DUP, 0x12)       /* duplicate top value on the stack */                                   \
-    _(OP_POP, 0x13)       /* remove top value off the stack */                                     \
-    _(OP_PUSHINT, 0x20)   /* push int value to the stack */                                        \
-    _(OP_PUSHSTR, 0x21)   /* push string value to the stack */                                     \
-    _(OP_PUSHTRUE, 0x22)  /* push the value `true` to the stack */                                 \
-    _(OP_PUSHFALSE, 0x23) /* push the value `false` to the stack */                                \
-    _(OP_MAKECLS, 0x30)   /* make closure from captures, N args and instruction address */         \
-    _(OP_CALLCLS, 0x31)   /* call closure with N args */                                           \
-    _(OP_ADD, 0x40)       /* pop two values off the stack and push their sum */                    \
-    _(OP_ISTUPLE, 0x50)   /* check if top value on the stack is a N-tuple (don't pop) */           \
-    _(OP_TUPLEGET, 0x51)  /* push N-th element of tuple on top of the stack (don't pop) */         \
-    _(OP_MAKETUPLE, 0x52) /* pop N values and make a tuple */                                      \
-    _(OP_MAKELIST, 0x53) /* push `nil` (empty list) and N times: pop 2 values and make a `cons` */ \
-    _(OP_ISNIL, 0x54)    /* check if top value on the stack is a `nil` (empty list) (don't pop) */ \
-    _(OP_ISCONS, 0x55)   /* check if top value on the stack is a `cons` (list) (don't pop) */      \
-    _(OP_UNCONS, 0x56)   /* pop top `cons` and push `tail` and `head` separately */                \
-    _(OP_CONCAT, 0x57)   /* pop 2 lists off the stack and push a concatenation */                  \
-    _(OP_LOADEXT, 0x80)  /* pop string off the stack and load an external symbol with that name */ \
-    _(OP_HALT, 0xFF)     /* stop execution */
+// clang-format off
+#define EACH_OPCODE(_)                                                                              \
+    _(OP_JUMP,      0x01) /* jump to relative address unconditionally */                            \
+    _(OP_JUMPIF,    0x02) /* pop a value and jump to relative address if it's `true` */             \
+    _(OP_JUMPIFN,   0x03) /* pop a value and jump to relative address if it's `false` */            \
+    _(OP_CALL,      0x04) /* look up function, push a frame, and jump */                            \
+    _(OP_RETURN,    0x05) /* pop a frame and restore instruction and stack pointers */              \
+    _(OP_LOAD,      0x10) /* load local */                                                          \
+    _(OP_STORE,     0x11) /* store local */                                                         \
+    _(OP_DUP,       0x12) /* duplicate top value on the stack */                                    \
+    _(OP_POP,       0x13) /* remove top value off the stack */                                      \
+    _(OP_PUSHINT,   0x20) /* push int value to the stack */                                         \
+    _(OP_PUSHSTR,   0x21) /* push string value to the stack */                                      \
+    _(OP_PUSHTRUE,  0x22) /* push the value `true` to the stack */                                  \
+    _(OP_PUSHFALSE, 0x23) /* push the value `false` to the stack */                                 \
+    _(OP_MAKECLS,   0x30) /* look up function, store N captures, and push closure onto the stack */ \
+    _(OP_CALLCLS,   0x31) /* call closure with N args */                                            \
+    _(OP_ADD,       0x40) /* pop two values off the stack and push their sum */                     \
+    _(OP_ISTUPLE,   0x50) /* check if top value on the stack is a N-tuple (don't pop) */            \
+    _(OP_TUPLEGET,  0x51) /* push N-th element of tuple on top of the stack (don't pop) */          \
+    _(OP_MAKETUPLE, 0x52) /* pop N values and make a tuple */                                       \
+    _(OP_MAKELIST,  0x53) /* push `nil` (empty list) and N times: pop 2 values and make a `cons` */ \
+    _(OP_ISNIL,     0x54) /* check if top value on the stack is a `nil` (empty list) (don't pop) */ \
+    _(OP_ISCONS,    0x55) /* check if top value on the stack is a `cons` (list) (don't pop) */      \
+    _(OP_UNCONS,    0x56) /* pop top `cons` and push `tail` and `head` separately */                \
+    _(OP_CONCAT,    0x57) /* pop 2 lists off the stack and push a concatenation */                  \
+    _(OP_LOADEXT,   0x80) /* pop string off the stack and load an external symbol with that name */ \
+    _(OP_HALT,      0xFF) /* stop execution */
+// clang-format on
 
 #define ENUM_MEMBER(name, byte) name = byte,
 typedef enum { EACH_OPCODE(ENUM_MEMBER) } opcode;
